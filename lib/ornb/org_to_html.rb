0fdb58b59d7319b66ce0d929914320468b296206
Shigeto R. Nishitani <shigeto_nishitani@me.com>
2019-04-07 16:46:45 +0900

    rev org_to_html

--- lib/ornb/org_to_html.rb
+++ lib/ornb/org_to_html.rb
@@ -1,7 +1,14 @@
-def org_to_html(*argv)
-Dir.glob(dirs).each do |file|
-  if File.extname(file)=='.org'
-    p command = "emacs #{file} --batch -f org-html-export-to-html --kill"
-    system command
+def org_to_html(dirs)
+  Dir.glob(dirs).each do |file|
+    if File.extname(file)=='.org'
+      ["/Users/bob/org-html-themes/setup/theme-readtheorg.setup",
+       "/Users/bob/Github/org-html-themes/setup/theme-readtheorg-local-daddygongon.setup",
+       "/Users/bob/org-html-themes/setup/theme-readtheorg-local-daddygongon.setup"].each do |orig|
+        replace = "/Users/bob/Github/ornb/lib/theme-readtheorg.setup"
+        File.write(file, File.read(file).gsub(orig,replace))
+      end
+      p command = "emacs #{file} --batch -f org-html-export-to-html --kill"
+      system command
+    end
   end
 end
