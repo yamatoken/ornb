cb7ee04e921a66a3ce98789294e88a4bf49ba48d
Shigeto R. Nishitani <shigeto_nishitani@me.com>
2019-04-07 13:23:33 +0900

    add org_to_html

--- lib/ornb.rb
+++ lib/ornb.rb
@@ -1,6 +1,7 @@
 require "ornb/version"
 require "ornb/ornb_thor"
 require "ornb/ornb_tree"
+require "ornb/org_to_html"
 
 module Ornb
   # Your code goes here...
--- lib/ornbgorg_to_html.rb
+++
@@ -1,7 +0,0 @@
-def org_to_html(*argv)
-Dir.glob(dirs).each do |file|
-  if File.extname(file)=='.org'
-    p command = "emacs #{file} --batch -f org-html-export-to-html --kill"
-    system command
-  end
-end
