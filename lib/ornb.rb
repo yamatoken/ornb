01c68bf8cfd568b5dd2761932de1a020334795bd
Shigeto R. Nishitani <shigeto_nishitani@me.com>
2019-04-08 09:32:21 +0900

    rev org_to_html and mv ornb_tree to tree

--- lib/ornb.rb
+++ lib/ornb.rb
@@ -1,6 +1,6 @@
 require "ornb/version"
 require "ornb/ornb_thor"
-require "ornb/ornb_tree"
+require "ornb/tree"
 require "ornb/org_to_html"
 
 module Ornb
--- lib/ornb/org_to_html.rb
+++ lib/ornb/org_to_html.rb
@@ -1,14 +1,19 @@
-def org_to_html(dirs)
-  Dir.glob(dirs).each do |file|
-    if File.extname(file)=='.org'
-      ["/Users/bob/org-html-themes/setup/theme-readtheorg.setup",
-       "/Users/bob/Github/org-html-themes/setup/theme-readtheorg-local-daddygongon.setup",
-       "/Users/bob/org-html-themes/setup/theme-readtheorg-local-daddygongon.setup"].each do |orig|
-        replace = "/Users/bob/Github/ornb/lib/theme-readtheorg.setup"
-        File.write(file, File.read(file).gsub(orig,replace))
+module Ornb
+  class CLI < Thor
+    private
+    def org_to_html(dirs)
+      replace = "/Users/bob/Github/ornb/lib/theme-readtheorg.setup"
+      Dir.glob(dirs).each do |file|
+        if File.extname(file)=='.org'
+          ["/Users/bob/org-html-themes/setup/theme-readtheorg.setup",
+           "/Users/bob/Github/org-html-themes/setup/theme-readtheorg-local-daddygongon.setup",
+           "/Users/bob/org-html-themes/setup/theme-readtheorg-local-daddygongon.setup"].each do |orig|
+            File.write(file, File.read(file).gsub(orig,replace))
+          end
+          p command = "emacs #{file} --batch -f org-html-export-to-html --kill"
+          system command
+        end
       end
-      p command = "emacs #{file} --batch -f org-html-export-to-html --kill"
-      system command
     end
   end
 end
