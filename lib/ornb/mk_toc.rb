aff435d1a344de48161a3933be81cd341c783f97
Shigeto R. Nishitani <shigeto_nishitani@me.com>
2019-04-08 12:55:55 +0900

    rev toc.rb

--- lib/ornb/mk_toc.rb
+++ lib/ornb/mk_toc.rb
@@ -3,19 +3,18 @@ module Ornb
     def mk_toc
       toc = ''
       Dir.glob('*') do |dir|
-        toc << "** [[./"+dir+"/]]\n"
-        if File.directory?(dir)
-          Dir.chdir(dir) do
-            Dir.glob('*') do |file|
-              next if File.extname(file)=='.org'
-              line = "- [["+File.join('.',dir,file)+"]["+file
-              terminal = File.directory?(file) ? "/]]\n" : "]]\n"
-              toc << line+terminal
-            end
-          end
-        end
+        toc << "** [[./"+dir+"/]]\n"
+        if File.directory?(dir)
+          Dir.chdir(dir) do
+            Dir.glob('*') do |file|
+              next if File.extname(file)=='.org'
+              line = "- [["+File.join('.',dir,file)+"]["+file
+              terminal = File.directory?(file) ? "/]]\n" : "]]\n"
+              toc << line+terminal
+            end
+          end
+        end
       end
-
       print toc
     end
   end
